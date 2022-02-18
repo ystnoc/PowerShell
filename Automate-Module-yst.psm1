c32b8fe4e6c828688dd6bcf8ce5be7b530461d68
ystnoc <96490927+ystnoc@users.noreply.github.com>
2022-02-18 15:06:21 -0700

    Update Automate-Module-yst.psm1

--- Automate-Module-yst.psm1
+++ Automate-Module-yst.psm1
@@ -562,16 +562,16 @@ Function Install-Automate {
             If ($InstallExitCode.ExitCode -eq 0) {
                 If (!$Silent) {Write-Verbose "The Automate Agent Installer Executed Without Errors"}
             } Else {
-                Write-Host "Automate Installer Exit Code: $InstallExitCode" -ForegroundColor Red
-                Write-Host "Automate Installer Logs: $LogFullPath" -ForegroundColor Red
-                Write-Host "The Automate Installer failed. Waiting 15 Seconds..." -ForegroundColor Red
+                Write-Host "Automate Installer Exit Code: $InstallExitCode" -ForegroundColor Yellow
+                Write-Host "Automate Installer Logs: $LogFullPath" -ForegroundColor Yellow
+                Write-Host "The Automate Installer failed. Waiting 55 Seconds..." -ForegroundColor Red
                 Start-Sleep -s 55
-                Write-Host "Installer will execute twice" -ForegroundColor Yellow
+                #Write-Host "Installer will execute twice" -ForegroundColor Yellow
                 $Date = (get-date -UFormat %Y-%m-%d_%H-%M-%S)
                 $LogFullPath = "$env:windir\Temp\Automate_Agent_$Date.log"
                 $InstallExitCode = (Start-Process -NoNewWindow -FilePath $SoftwareFullPath -ArgumentList $arguments -Wait) 
-                Write-Host "Automate Installer Exit Code: $InstallExitCode" -ForegroundColor Yellow
-                Write-Host "Automate Installer Logs: $LogFullPath" -ForegroundColor Yellow
+                #Write-Host "Automate Installer Exit Code: $InstallExitCode" -ForegroundColor Yellow
+                #Write-Host "Automate Installer Logs: $LogFullPath" -ForegroundColor Yellow
             }# End Else
         If ($InstallExitCode.ExitCode -eq 0) {
             While ($Counter -ne 30) {
